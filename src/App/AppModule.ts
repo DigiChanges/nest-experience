dfe5731abfa1d7e54acca3dcd386f543c68cbd6f
Rutito2010 <kakaroto11@live.com.ar>
2024-07-18 11:49:07 -0300

    remove personal mongoose credentials

--- src/App/AppModule.ts
+++ src/App/AppModule.ts
@@ -5,14 +5,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    //MongooseModule.forRoot('mongodb://experience:experience@localhost:27018/experience'),
-    //TODO remove before commit
-    MongooseModule.forRoot(
-      'mongodb+srv://Rutito2010:Kaori-chan2010@cluster0.cvyj8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    ),
-
-    ItemModule,
+    MongooseModule.forRoot('mongodb://experience:experience@localhost:27018/experience'),
+    ItemModule
   ],
-  controllers: [AppController],
+  controllers: [AppController]
 })
 export class AppModule {}
