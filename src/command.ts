0a36dbfbdf198f850d41ad3035d2f88057b95286
Alexis Graff <alexisgraff123@gmail.com>
2024-07-30 09:36:21 -0300

    feat: add logger to CommandFactory.run call
    This change imports the Logger from '@nestjs/common' and initializes it before passing it to CommandFactory.run. This allows the application to log messages during the command execution process, enhancing debugging and monitoring capabilities.

--- src/command.ts
+++ src/command.ts
@@ -1,7 +1,9 @@
 import { CommandFactory } from 'nest-commander';
 import { AppModule } from '@src/App/AppModule';
+import { Logger } from '@nestjs/common';
 
 void (async() =>
 {
-  await CommandFactory.run(AppModule);
+  const logger = new Logger();
+  await CommandFactory.run(AppModule, logger);
 })();
