55ee9d5a678a1400bca35172b49f53d9c02d77ed
Alexis Graff <alexisgraff123@gmail.com>
2024-07-30 09:42:29 -0300

    feat: deleting TODO.

--- src/Auth/Presentation/Commands/SyncRolesPermissionCommander.ts
+++ src/Auth/Presentation/Commands/SyncRolesPermissionCommander.ts
@@ -1,6 +1,7 @@
 import { Command, CommandRunner } from 'nest-commander';
 import { CommandBus } from '@nestjs/cqrs';
 import SyncPermissionsCommand from '../../Application/Commands/SyncPermissionsCommand';
+
 @Command({ name: 'syncRolesPermission', description: 'Sync permissions' })
 export class SyncRolesPermissionCommander extends CommandRunner
 {
@@ -10,7 +11,6 @@ export class SyncRolesPermissionCommander extends CommandRunner
   }
   async run(): Promise<void>
   {
-    // TODO: Logger doesnt work, we need to config it.
     await this.commandBus.execute(new SyncPermissionsCommand());
   }
 }
