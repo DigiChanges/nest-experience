cba27820a8035efc6f41f80535bb76c66dbb40ef
Alexis Graff <alexisgraff123@gmail.com>
2024-08-28 16:43:07 -0300

    feat: implemented MongoMemoryServer
    Replace the static MongoDB URI with MongoMemoryServer for an in-memory database setup in tests. Updated package.json to include the mongodb-memory-server dependency and modified test cases to start and stop the in-memory server accordingly.

--- src/Item/Test/item.spec.ts
+++ src/Item/Test/item.spec.ts
@@ -4,14 +4,17 @@ import { ItemModule } from '@src/Item/ItemModule';
 import { getTestAgent } from '@src/Config/TestConfig';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { SharedModule } from '@shared/SharedModule';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 describe('ItemModule (e2e)', () =>
 {
   let app: NestFastifyApplication;
   let agent: TestAgent;
+  let mongoServer: MongoMemoryServer;
 
   beforeEach(async() =>
   {
+    mongoServer = await MongoMemoryServer.create();
     const config = await getTestAgent(SharedModule, ItemModule);
     app = config.app;
     agent = config.agent;
@@ -20,12 +23,42 @@ describe('ItemModule (e2e)', () =>
   afterAll(async() =>
   {
     await app.close();
+    await mongoServer.stop();
   });
 
-  test('/ (GET)', async() =>
+  test('Add Item /items', async() =>
   {
-    const response = await agent.get('/api/items');
+    const payload = {
+      name: 'Item 1',
+      description: 10
+    };
 
+    const response = await agent
+      .post('/api/items')
+      .set('Accept', 'application/json')
+      .send(payload);
+
+    expect(response.statusCode).toStrictEqual(201);
+    expect(response.body).toEqual({ message: 'Item created.' });
+  });
+
+  test('Get Items /items with pagination', async() =>
+  {
+    const response = await agent.get('/api/items?pagination[offset]=0&pagination[limit]=5');
+    const { body: { data, pagination } } = response;
+    console.log(pagination);
     expect(response.statusCode).toEqual(200);
+    expect(data.length).toEqual(0);
+    expect(pagination.total).toEqual(0);
+    expect(pagination.perPage).toEqual(0);
+    expect(pagination.currentPage).toEqual(1);
+    expect(pagination.lastPage).toEqual(0);
+    expect(pagination.from).toEqual(0);
+    expect(pagination.to).toEqual(0);
+    expect(pagination.firstUrl).toContain('/api/items?pagination[offset]=0&pagination[limit]=5');
+    expect(pagination.lastUrl).toContain('/api/items?pagination[offset]=-5&pagination[limit]=5');
+    expect(pagination.nextUrl).toBeNull();
+    expect(pagination.prevUrl).toBeNull();
+    expect(pagination.currentUrl).toContain('/api/items?pagination[offset]=0&pagination[limit]=5');
   });
 });
