602c117c5258c2478c046dfa3e993f31d19dbba8
Mariel Torres <marieltorres97@gmail.com>
2024-10-16 20:06:09 -0300

    feat: add global filter to app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { AppModule } from '@app/AppModule';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AllExceptionsFilter } from '@shared/Exceptions/AllExceptionsFilter';
 import App from '@src/appFactory';
 
 void (async() =>
@@ -10,9 +9,11 @@ void (async() =>
     AppModule,
     new FastifyAdapter()
   );
+
   const application = new App(app);
+
   application.init();
-  app.useGlobalFilters(new AllExceptionsFilter());
+  application.useGlobalFilters();
   await application.initMiddleware();
   await application.listen(8089);
 })();
